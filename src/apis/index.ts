211364a250831c30e223db0fb046565a62466c76
Abhishek <abhipatel8719@gmail.com>
2022-03-05 01:30:41 +0530

    feature/axios-added

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,5 +1,30 @@
 import axios from "axios";
 
-const api = axios.create({
+export const api = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
 });
+
+// For GET requests
+api.interceptors.request.use(
+  (req) => {
+    // Add configurations here
+    return req;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+// For POST requests
+api.interceptors.response.use(
+  (res) => {
+    // Add configurations here
+    if (res.status === 201) {
+      console.log("Posted Successfully");
+    }
+    return res;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
